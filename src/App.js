c548f1d23212cb0e60f10c5e57c81f746b3874b5
t.nguen <t.nguen@city-mobil.ru>
2021-05-06 23:37:30 +0300

    logo

--- src/App.js
+++ src/App.js
@@ -6,16 +6,34 @@ import { TasksBoard } from "./layouts/tasksBoard/tasksBoard";
 
 import { ProtectedRoute } from "./components/protectedRoute/protectedRoute";
 
+import "./app.scss";
+import logoSvg from "./logo.svg";
+
 function App() {
   const history = useHistory();
-  const [isAuth, setIsAuth] = useState(true);
+  const [isAuth, setIsAuth] = useState(false);
 
   useEffect(() => {
-    isAuth && history.push("/tasks/1");
+    // isAuth && history.push("/tasks/1");
   }, []);
 
   return (
     <div className="App">
+      <div className="logo">
+        <div>
+          <img src={logoSvg} />
+          <p className="logo-smile">: )</p>
+        </div>
+
+        <p
+          style={{
+            fontSize: 39,
+            fontWeight: "bold",
+          }}
+        >
+          Task-Tracker
+        </p>
+      </div>
       <Switch>
         <Route exact path="/" component={Authentication} />
         <ProtectedRoute path="/tasks/:id" isAuth={isAuth} component={TasksBoard} />
